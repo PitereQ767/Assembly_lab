b2118c5c0d81f1d641cdff56023fee79e4a57238
Piotr <ratkowskipiotr04@gmail.com>
2025-12-07 00:03:47 +0100

    softmax function repair

--- Lab5/Lab5_projekt/Project1/Project1/lab_funkcje.asm
+++ Lab5/Lab5_projekt/Project1/Project1/lab_funkcje.asm
@@ -1,12 +1,37 @@
 .686
 .model flat
+.xmm
 
-public _softmax
+public _softmax, _szybki_max
 
 .data
 	suma dd 0.0
 
 .code
+
+_e_do_x PROC
+
+	fldl2e
+	fmulp st(1), st(0) ;st(0) = x * log2x
+	fst st(1)
+
+	frndint 
+
+	fsub st(1), st(0); ;st(0) = czesc calkowita, st(1) = czesc ulamkowa
+
+	fxch ;st(0) = czesc ulamkowa, st(1) = czesc calkowita
+
+	f2xm1
+	fld1
+	faddp st(1), st(0)
+
+	fscale
+	
+	fstp st(1)
+
+
+	ret
+_e_do_x ENDP
 _softmax PROC
 	push ebp
 	mov ebp, esp
@@ -17,72 +42,84 @@ _softmax PROC
 	mov esi, [ebp + 8]
 	mov ebx, [ebp + 12]
 	mov edi, [ebp + 16]
-	dec edi
-	mov eax, edi ; przechowujemy n - 1
+	
+	xor ecx, ecx
 
-	finit
 petla:
-	fld dword ptr [esi + 4*edi]
-	fldl2e ;stos st(0) = log2e, st(1) = x
-	fmulp st(1), st(0) ;stos st(0) = x * log2e
-	fst st(1)
-	frndint ;zaokraglenie wartosci do liczby calkowitej
+	cmp ecx, edi
+	jge koniec_sumy
 
-	fsub st(1), st(0) ; stos: st(0) = (x * log2e)zaokraglone calkowite, st(1) = czesc ulamkowa (x * log2e)
-
-	fxch ; stos: st(0) = czesc ulamkowa (x * log2e) , st(1) = (x * log2e)zaokraglone calkowite
-
-	f2xm1 ;stos: st(0) = 2^ czesc ulamkowa (x * log2e) , st(1) = (x * log2e)zaokraglone calkowite
-	fld1
-	faddp st(1), st(0)
-
-	fscale
-
+	fld dword ptr [esi + 4*ecx]
+	call _e_do_x
 	fld suma
 	faddp st(1), st(0)
+
 	fst suma
 
-	fstp st(0)
-	fstp st(0)
+	inc ecx
+	jmp petla
 
-	dec edi
-	cmp edi, 0
-	jge petla
 
-	mov edi, eax
+koniec_sumy:
+	xor ecx, ecx
+
 petla_2:
-	fld dword ptr [esi + 4*edi]
-	fldl2e ;stos st(0) = log2e, st(1) = x
-	fmulp st(1), st(0) ;stos st(0) = x * log2e
-	fst st(1)
-	frndint ;zaokraglenie wartosci do liczby calkowitej
+	cmp ecx, edi
+	jge koniec
 
-	fsub st(1), st(0) ; stos: st(0) = (x * log2e)zaokraglone calkowite, st(1) = czesc ulamkowa (x * log2e)
-
-	fxch ; stos: st(0) = czesc ulamkowa (x * log2e) , st(1) = (x * log2e)zaokraglone calkowite
-
-	f2xm1 ;stos: st(0) = 2^ czesc ulamkowa (x * log2e) , st(1) = (x * log2e)zaokraglone calkowite
-	fld1
-	faddp st(1), st(0)
-
-	fscale
-	fstp st(1)
-
+	fld dword ptr [esi + 4*ecx]
+	call _e_do_x
 	fld suma
 	fdivp st(1), st(0)
+	fstp dword ptr [ebx + 4*ecx]
 
-	fst dword ptr [ebx + 4*edi]
-	dec edi
-	cmp edi, 0
-	jge petla_2
-
+	inc ecx
+	jmp petla_2
 
 
+koniec:
+	
 	pop ebx
 	pop edi
 	pop esi
 	pop ebp
 	ret
 _softmax ENDP
+
+_szybki_max PROC
+	push ebp
+	mov ebp, esp
+	push edi
+	push esi
+	push ebx
+
+	mov esi, [ebp + 8]
+	mov edi, [ebp + 12]
+	mov ebx, [ebp + 16]
+	mov ecx, [ebp + 20]
+
+	movups xmm5, [esi]
+	movups xmm6, [edi]
+
+	pmaxsw xmm5, xmm6
+
+	movups [ebx], xmm5
+
+	movups xmm5, [esi + ecx]
+	movups xmm6, [edi + ecx]
+
+	pmaxsw xmm5, xmm6
+
+	movups [ebx + ecx], xmm5
+
+
+
+	pop ebx
+	pop edi
+	pop esi
+
+	pop ebp
+	ret
+_szybki_max ENDP
 
 END
--- Lab5/Lab5_projekt/Project1/Project1/main_lab.c
+++ Lab5/Lab5_projekt/Project1/Project1/main_lab.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 float softmax(float v[], float wynik[], unsigned int n);
+void szybki_max(short int t_1[], short int t_2[], short int t_wynik[], int n);
 
 int main() {
 	//float v[3] = { 1.0f, 2.0f, 3.0f };
 	//float wynik[3];
+	//softmax(v, wynik, 3);
 	//printf("\nWynik: %f %f %f", wynik[0], wynik[1], wynik[2]);
 
 	float v[10] = { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 1.0f, 0.0f, -1.0f, 2.0f, 3.0f };
 	float wynik[10];
 	softmax(v, wynik, 10);
 	printf("\nWynik: %f %f %f %f %f %f %f %f %f %f", wynik[0], wynik[1], wynik[2], wynik[3], wynik[4], wynik[5], wynik[6], wynik[7], wynik[8], wynik[9]);
+
+	/*short int val1[16] = { 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6, 7, -7, -8, 8 };
+	short int val2[16] = { -3, -2, -1, 0, 1, 2, 3, 4, 256, -256, 257, -257, 258, -258, 0, 0 };
+	short int wynik[16];
+	szybki_max(val1, val2, wynik, 16);
+	printf("\nWynik: %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd %hd", wynik[0], wynik[1], wynik[2], wynik[3], wynik[4], wynik[5], wynik[6], wynik[7], wynik[8], wynik[9], wynik[10], wynik[11], wynik[12], wynik[13], wynik[14], wynik[15]);*/
 
 
 	return 0;
