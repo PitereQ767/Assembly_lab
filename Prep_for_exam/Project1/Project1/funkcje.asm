20c32e72d31d0f99d90b1a56f855589e5b452349
Piotr <ratkowskipiotr04@gmail.com>
2025-12-29 15:02:26 +0100

    Zadanie8

--- Prep_for_exam/Project1/Project1/funkcje.asm
+++ Prep_for_exam/Project1/Project1/funkcje.asm
@@ -1,11 +1,12 @@
 .686
 .model flat
 
-public _roznica, _kopia_tablicy
+public _roznica, _kopia_tablicy, _komunikat, _szukaj_elem_min, _szyfruj, _kwadrat, _iteracja, _zadanie8
 
 extern _malloc:PROC
 
 .data
+	cztery dd 4
 
 .code
 _roznica PROC
@@ -33,13 +34,20 @@ _kopia_tablicy PROC
 	push ebp
 	mov ebp, esp
 	push ebx
+	push esi
+	push edi
 
 	mov ebx, [ebp + 8]
 	mov ecx, [ebp + 12]
+	
+	mov edi, ecx
+	imul edi, 4
 
+	push edi
 	push ecx
 	call _malloc
 	pop ecx
+	add esp, 4
 
 	mov edi, 0
 petla:
@@ -61,11 +69,228 @@ koniec_petli:
 
 koniec:
 	
-
+	pop edi
+	pop esi
 	pop ebx
 	pop ebp
 	ret
 _kopia_tablicy ENDP
+
+_komunikat PROC
+	push ebp
+	mov ebp, esp
+	push ebx
+	push edi
+
+	mov ebx, [ebp + 8]
+
+	xor ecx, ecx
+
+licznik:
+	mov eax, [ebx + ecx]
+	cmp eax, 0
+	je koniec_liczenia
+	inc ecx
+	jmp licznik
+
+
+koniec_liczenia:
+	inc ecx
+	push ecx
+	call _malloc
+	pop ecx
+
+	xor edi, edi
+petla:
+	cmp edi, ecx
+	je koniec
+	movzx edx, byte ptr [ebx + edi]
+	mov [eax + edi], dl
+	inc edi
+	jmp petla
+
+koniec:
+	dec ecx
+	mov byte ptr [eax + ecx], 'B'
+	inc ecx
+	mov word ptr [eax + ecx], '³'
+	inc ecx
+	mov word ptr [eax + ecx], '¹'
+	inc ecx
+	mov byte ptr [eax + ecx], 'd'
+	inc ecx
+	mov byte ptr [eax + ecx], 0
+	inc ecx
+	
+	pop edi
+	pop ebx
+	pop ebp
+	ret
+_komunikat ENDP
+
+_szukaj_elem_min PROC
+	push ebp
+	mov ebp, esp
+	push ebx
+	push edi
+
+	mov ebx, [ebp + 8]
+	mov ecx, [ebp + 12]
+
+	xor edi, edi
+
+	mov eax, ebx ;uznajemy ze pierwszy jest najmniejszy
+	inc edi
+
+petla:
+	cmp edi, ecx
+	jge koniec
+	mov edx, [ebx + 4*edi]
+	cmp [eax], edx
+	jbe mniejsza
+
+	lea eax, [ebx + 4*edi]
+
+mniejsza:
+	inc edi
+	jmp petla
+
+
+koniec:
+	
+	pop edi
+	pop ebx
+	pop ebp
+	ret
+_szukaj_elem_min ENDP
+
+_szyfruj PROC
+	push ebp
+	mov ebp, esp
+	push ebx
+	push edi
+	push esi
+
+
+	mov ebx, [ebp + 8]
+	mov edx, 52525252h
+
+petla:
+	cmp byte ptr [ebx], 0
+	je koniec
+	xor [ebx], dl
+
+	bt edx, 30
+	setc al
+	bt edx, 31
+	setc cl
+
+	xor al, cl
+
+	cmp al, 0
+	je zgadza_sie
+	stc
+	jmp koniec_por
+zgadza_sie:
+	clc
+koniec_por:
+	rcl edx, 1
+	inc ebx
+	jmp petla
+
+
+koniec:
+
+	pop esi
+	pop edi
+	pop ebx
+	pop ebp
+	ret
+_szyfruj ENDP
+
+_kwadrat PROC
+	push ebp
+	mov ebp, esp
+	push ebx
+	push edi
+	push esi
+
+	mov ecx, [ebp + 8]
+
+	cmp ecx, 1
+	je koniec
+
+	cmp ecx, 0
+	je koniec
+
+	push ecx
+	sub ecx, 2
+	push ecx
+	call _kwadrat
+	add esp, 4
+	pop eax
+	mul cztery
+	sub eax, 4
+	add ecx, eax
+
+	
+
+
+koniec:
+	mov eax, ecx
+
+	pop esi
+	pop edi
+	pop ebx
+	pop ebp
+	ret
+_kwadrat ENDP
+
+_iteracja PROC 
+	push ebp 
+	mov ebp, esp 
+	mov al, [ebp+8] 
+	sal al, 1 
+	jc zakoncz 
+	inc al 
+	push eax
+	call _iteracja 
+	add esp, 4 
+	pop ebp 
+	ret 
+	zakoncz: 
+	rcr al, 1 
+	pop ebp 
+	ret 
+_iteracja ENDP 
+
+_zadanie8 PROC
+	push ebp
+	mov esp, ebp
+	push esi
+	push edi
+	push ebx
+
+	mov ebx, 00000080h
+
+	shr ebx, 7
+
+	cmp ebx, 0
+	je zero
+	stc
+	jmp koniec
+
+zero:
+	clc
+
+koniec:
+
+	pop ebx
+	pop edi
+	pop esi
+	pop ebx
+	ret
+_zadanie8 ENDP
 
 
 END
--- Prep_for_exam/Project1/Project1/main.c
+++ Prep_for_exam/Project1/Project1/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <locale.h>
 
 int roznica(int* odejmna, int** odejmnik);
 int* kopia_tablicy(int tab1[], unsigned int n);
+char* komunikat(char* tekst);
+int* szukaj_elem_min(int tablica[], int n);
+void szyfruj(char* tekst);
+unsigned int kwadrat(unsigned int a);
+unsigned char iteracja(unsigned char a);
+void zadanie8();
 
 
 int main() {
+	setlocale(LC_ALL, "Polish");
+
 	printf("\n------------------Zadanie1------------------\n");
 	int a, b, * wsk, wynik;
 	wsk = &b;
@@ -20,6 +29,36 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		printf("%d ", tab2[i]);
 	}
+
+	printf("\n------------------Zadanie3------------------\n");
+	char* tekst = "AKO³up";
+	char* tekst_2 = komunikat(tekst);
+	printf("Wynik: %s", tekst_2);
+
+
+	printf("\n------------------Zadanie4------------------\n");
+	int pomiary[7] = { 3, 2, 6, 7, 9, 4, 1 }, * wsk_2;
+	wsk_2 = szukaj_elem_min(pomiary, 7);
+	printf("Element minimalny = %d", *wsk_2);
+
+	printf("\n------------------Zadanie5------------------\n");
+	char tekst_szyfr[] = "To jest jakis tekst";
+	szyfruj(tekst_szyfr);
+	printf("Zaszyfrowany tekst: %s", tekst_szyfr);
+
+	printf("\n------------------Zadanie6------------------\n");
+	unsigned int x = 65000;
+	unsigned int y = kwadrat(x);
+	printf("Wynik rekurencyjnej funkcji kwadrat wynosi: %u", y);
+
+	printf("\n------------------Zadanie7------------------\n");
+	char w = iteracja(32);
+	printf("Znak : %c", w);
+
+	printf("\n------------------Zadanie8------------------\n");
+	zadanie8();
+
+
 
 	return 0;
 }
